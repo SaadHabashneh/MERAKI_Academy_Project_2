3c47c354c3cb1cbad6cba44bc95c03fa67f40cf6
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2023-07-16 05:03:39 +0300

    added more data

--- main.js
+++ main.js
@@ -82,19 +82,19 @@ favDiv.hide();
 
 // each genre function:-
 
-// action function:-
+// action button function:-
 
 
 
-// comedy function:-
+// comedy button function:-
 
 
 
-// horror function:-
+// horror button function:-
 
 
 
-// drama function:-
+// drama button function:-
 
 
 
@@ -112,52 +112,172 @@ topRated.hide();
 
 const descriptionDiv = $(`<div id="desDiv"></div>`);
 body.append(descriptionDiv);
+
 const descriptionTitle = $(`<h2 id="desTitle"></h2>`);
 descriptionDiv.append(descriptionTitle);
+
 const trailer = $(`<a href="" id="trailer">Watch Trailer</a>`);
 descriptionDiv.append(trailer);
+
 const actors = $(`<strong id="actors"></strong>`);
 descriptionDiv.append(actors);
+
 const info = $(`<p id="movieInfo"></p>`);
 descriptionDiv.append(info);
+
+const addFavBtn = $(`<button class="favDeleteBtn">Add to favorites</button>`);
+descriptionDiv.append(addFavBtn);
 descriptionDiv.hide();
 
 // action movies data:-
 
 const actionMovies = [
-{
-    id: 1,
-    movieName: "Sound of Freedom",
-    actors: ["Jim Caviezel", "Mira Sorvino", "Bill Camp", "Cristal Aparicio"],
-    imageSrc: "./images & icons/sound-of-freedom",
-    description: "The incredible true story of a former government agent turned vigilante who embarks on a dangerous mission to rescue hundreds of children from sex traffickers.",
-    rate: 4.3,
-},
-{
-    id: 2,
-    movieName: "Extraction II",
-    actors: ["Chris Hemsworth", "Golshifteh Farahani", "Adam Bessa", "Tornike Gogrichiani"],
-    imageSrc: "./images & icons/Extraction-2",
-    description: "After barely surviving his grievous wounds from his mission in Dhaka, Bangladesh, Tyler Rake is back, and his team is ready to take on their next mission.",
-    rate: 3.6,
-},
-{
-    id: 3,
-    movieName: "John Wick: Chapter 4",
-    actors: ["Keanu Reeves", "Laurence Fishburne", "George Georgiou", "Lance Reddick"],
-    imageSrc: "./images & icons/john-wick",
-    description: "John Wick uncovers a path to defeating The High Table. But before he can earn his freedom, Wick must face off against a new enemy with powerful alliances across the globe and forces that turn old friends into foes.",
-    rate: 4,
-},
-{
-    id: 4,
-    movieName: " Top Gun: Maverick",
-    actors: ["Tom Cruise", "Jennifer Connelly", "Miles Teller", "Val Kilmer"],
-    imageSrc: "./images & icons/top-gun-maverick",
-    description: "After thirty years, Maverick is still pushing the envelope as a top naval aviator, but must confront ghosts of his past when he leads TOP GUN's elite graduates on a mission that demands the ultimate sacrifice from those chosen to fly it.",
-    rate: 4.3,
-}
+    {
+        id: 1,
+        movieName: "Sound of Freedom",
+        actors: ["Jim Caviezel", "Mira Sorvino", "Bill Camp", "Cristal Aparicio"],
+        imageSrc: "./images & icons/sound-of-freedom",
+        description: "The incredible true story of a former government agent turned vigilante who embarks on a dangerous mission to rescue hundreds of children from sex traffickers.",
+        rate: 4.3,
+    },
+    {
+        id: 2,
+        movieName: "Extraction II",
+        actors: ["Chris Hemsworth", "Golshifteh Farahani", "Adam Bessa", "Tornike Gogrichiani"],
+        imageSrc: "./images & icons/Extraction-2",
+        description: "After barely surviving his grievous wounds from his mission in Dhaka, Bangladesh, Tyler Rake is back, and his team is ready to take on their next mission.",
+        rate: 3.6,
+    },
+    {
+        id: 3,
+        movieName: "John Wick: Chapter 4",
+        actors: ["Keanu Reeves", "Laurence Fishburne", "George Georgiou", "Lance Reddick"],
+        imageSrc: "./images & icons/john-wick",
+        description: "John Wick uncovers a path to defeating The High Table. But before he can earn his freedom, Wick must face off against a new enemy with powerful alliances across the globe and forces that turn old friends into foes.",
+        rate: 4,
+    },
+    {
+        id: 4,
+        movieName: "Top Gun: Maverick",
+        actors: ["Tom Cruise", "Jennifer Connelly", "Miles Teller", "Val Kilmer"],
+        imageSrc: "./images & icons/top-gun-maverick",
+        description: "After thirty years, Maverick is still pushing the envelope as a top naval aviator, but must confront ghosts of his past when he leads TOP GUN's elite graduates on a mission that demands the ultimate sacrifice from those chosen to fly it.",
+        rate: 4.3,
+    }
 ];
 // for the `imageSrc` it can use an array of url/path
 
+// comedy movies data:-
+
+const comedyMovies = [
+    {
+        id: 1,
+        movieName: "Glass Onion",
+        actors: ["Daniel Craig", "Edward Norton", "Kate Hudson", "Dave Bautista"],
+        imageSrc: "./images & icons/glass-onions",
+        description: "Tech billionaire Miles Bron invites his friends for a getaway on his private Greek island. When someone turns up dead, Detective Benoit Blanc is put on the case.",
+        rate: 3.6,
+    },
+    {
+        id: 2,
+        movieName: "We're the Millers",
+        actors: ["Jason Sudeikis", "Jennifer Aniston", "Emma Roberts", "Ed Helms"],
+        imageSrc: "./images & icons/we're-the-millers",
+        description: "A veteran pot dealer creates a fake family as part of his plan to move a huge shipment of weed into the U.S. from Mexico.",
+        rate: 3.5,
+    },
+    {
+        id: 3,
+        movieName: "Puss in Boots: The Last Wish",
+        actors: ["Antonio Banderas", "Salma Hayek", "Harvey Guillén", "Florence Pugh"],
+        imageSrc: "./images & icons/puss-in-boots",
+        description: "When Puss in Boots discovers that his passion for adventure has taken its toll and he has burned through eight of his nine lives, he launches an epic journey to restore them by finding the mythical Last Wish.",
+        rate: 4,
+    },
+    {
+        id: 4,
+        movieName: "The Unbearable Weight of Massive Talent",
+        actors: ["Nicolas Cage", "Pedro Pascal", "Tiffany Haddish", "Sharon Horgan"],
+        imageSrc: "./images & icons/unbearable-weight",
+        description: "Moviestar Nick Cage is channeling his iconic characters as he's caught between a superfan and a CIA agent.",
+        rate: 3.5,
+    }
+];
+// for the `imageSrc` it can use an array of url/path
+
+// horror movies data:-
+
+const horrorMovies = [
+    {
+        id: 1,
+        movieName: "Insidious: The Red Door",
+        actors: ["Ty Simpkins", "Patrick Wilson", "Rose Byrne", "Sinclair Daniel"],
+        imageSrc: "./images & icons/insidious",
+        description: "The Lamberts must go deeper into The Further than ever before to put their demons to rest once and for all.",
+        rate: 3,
+    },
+    {
+        id: 2,
+        movieName: "M3gan",
+        actors: ["Allison Williams", "Violet McGraw", "Ronny Chieng", "Amie Donald"],
+        imageSrc: "./images & icons/m3gan",
+        description: "A robotics engineer at a toy company builds a life-like doll that begins to take on a life of its own.",
+        rate: 3.2,
+    },
+    {
+        id: 3,
+        movieName: "Scream VI",
+        actors: ["Courteney Cox", "Melissa Barrera", "Jenna Ortega", "Jasmin Savoy Brown"],
+        imageSrc: "./images & icons/scream-6",
+        description: "In the next installment, the survivors of the Ghostface killings leave Woodsboro behind and start a fresh chapter in New York City.",
+        rate: 3.3,
+    },
+    {
+        id: 4,
+        movieName: "Terrifier 2",
+        actors: ["Lauren LaVera", "David Howard Thornton", "Elliott Fullam", "Sarah Voigt"],
+        imageSrc: "./images & icons/terrifier-2",
+        description: "After being resurrected by a sinister entity, Art the Clown returns to the timid town of Miles County where he targets a teenage girl and her younger brother on Halloween night.",
+        rate: 3.1,
+    }
+];
+// for the `imageSrc` it can use an array of url/path
+
+// drama movies data:-
+
+const dramaMovies = [
+    {
+        id: 1,
+        movieName: "Oppenheimer",
+        actors: ["Cillian Murphy, Emily Blunt, Matt Damon, Robert Downey Jr."],
+        imageSrc: "./images & icons/oppenheimer",
+        description: "The story of American scientist J. Robert Oppenheimer and his role in the development of the atomic bomb.",
+        rate: 4,
+    },
+    {
+        id: 2,
+        movieName: "Interstellar",
+        actors: [" Matthew McConaughey, Anne Hathaway, Jessica Chastain, Mackenzie Foy"],
+        imageSrc: "./images & icons/interstellar",
+        description: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
+        rate: 4.6,
+    },
+    {
+        id: 3,
+        movieName: "The Shawshank Redemption",
+        actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
+        imageSrc: "./images & icons/the-shawshank",
+        description: "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",
+        rate: 4.8,
+    },
+    {
+        id: 4,
+        movieName: "Blade Runner 2049",
+        actors: ["Harrison Ford", "Ryan Gosling", "Ana de Armas", "Dave Bautista"],
+        imageSrc: "./images & icons/blade-runner",
+        description: "Young Blade Runner K's discovery of a long-buried secret leads him to track down former Blade Runner Rick Deckard, who's been missing for thirty years.",
+        rate: 4,
+    }
+];
+// for the `imageSrc` it can use an array of url/path
 
